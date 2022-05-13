49bfb6d53ddbcf9d6a30ea65bd8db012076fb8c1
AugustoN9 <auguston8@hotmail.com>
2022-05-12 23:19:14 -0300

    Subindo arquivos

---
+++ 1-meuPrimeiroPrograma.js
@@ -0,0 +1,15 @@
+console.log("Meu Primeiro Programa JS:trabalhando com variáveis");
+
+const idade = 26;
+
+idade => idade + 2;
+
+console.log("idade");
+console.log(idade);
+console.log("adicao +2",idade+2);
+console.log("subtracao -2",idade-2);
+console.log("divisao /2",idade/2);
+console.log("multiplicao *2",idade*2);
+
+const idadesomada = idade + 7;
+console.log(idadesomada);
---
+++ 2-trabalhandoComVariaveis.js
@@ -0,0 +1,10 @@
+console.log("Trabalhando com variáveis");
+//JS é case sensitive
+
+idade = 29;
+Idade = 30;
+
+const idadeUsuario = 41;
+const nome = "Ricardo";
+
+console.log(idade);
